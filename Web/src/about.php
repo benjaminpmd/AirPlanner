cd65600ff9d25df892651b1eb7d94a7cb68169b3
bennjaerospace <dev.benjaminpaumard@gmail.com>
2022-11-27 13:25:41 +0100

    adding some page content

--- Web/src/about.php
+++ Web/src/about.php
@@ -5,8 +5,11 @@ $page_canonical = "/about.php";
 
 include "./include/header.inc.php";
 ?>
-    <section>
-        <h2 class="text-center text-2xl">Page en cours de construction</h2>
+    <section class="mx-10 md:m-auto md:max-w-3xl p-5 text-center rounded-xl items-center object-center bg-gray-200 dark:bg-gray-700">
+        <h2 class="text-center text-2xl">Qui sommes nous</h2>
+            <p>Nous sommes 3 étudiants en 3ème année de licence d'informatique à l'université de Cergy-Pontoise.</p>
+            <p>Dans le cadre d'un projet de gestion de base de données et de réseau, nous avons choisi comme projet de créer 
+            un outil de gestion de reservation de vols pour un aéroclub. </p>
     </section>
 <?php
 include "./include/footer.inc.php";
--- Web/src/privacy-policy.php
+++ Web/src/privacy-policy.php
@@ -5,11 +5,18 @@ $page_canonical = "/privacy-policy.php";
 
 include "./include/header.inc.php";
 ?>
-    <section>
+    <section class="mx-10 md:m-auto md:max-w-3xl p-5 text-center rounded-xl items-center object-center bg-gray-200 dark:bg-gray-700">
         <h2 class="text-center text-2xl p-5">Politique appliquée au 1er Novembre 2022</h2>
         <article>
-            <h3>Préambule</h3>
-            <p></p>
+            <h3 class="text-center text-xl p-3">Protection de vos données personnelles</h3>
+                <h4 class="text-center text-2xl p-2">Pourquoi recueillir vos informations</h4>
+                    <p>AirPlanner recueille certaines de vos informations afin d'assurer le bon fonctionnement du site</p>
+                    <p>Elles ne sont en aucun cas utilisées à des fins commerciales</p>
+
+                <h4 class="text-center text-2xl p-5">Combien de temps les données sont-elles conservées</h4>
+                    <p>Les informations stockées dans les cookies ne restent que le temps de votre session et sont supprimées dès la déconnexion.</p>
+                    <p>En revanche, les données liées au profil utilisateur ainsi que les données liées aux services du site sont stockées dans 
+                    la base de données afin d'être traitées et d'envoyer les bonnes informations à jour.</p>
         </article>
     </section>
 <?php
